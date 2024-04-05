5f251274e222641f1fdbbde1bb72a121962e93bd
im6h <danghaiv.18@gmail.com>
2024-04-05 22:57:58 +0700

    fix: update color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   -- add gruvbox
   { "wittyjudge/gruvbox-material.nvim" },
+  --{ "shaunsingh/nord.nvim" },
+  { "gbprod/nord.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "nord", --"gruvbox-material",
     },
   },
 }
